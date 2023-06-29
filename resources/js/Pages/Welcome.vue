ae9f28d6b8e7ab00020b8fe85a0ec784935561f7
Ričards <ricards.grinbergs@outlook.com>
2023-06-29 19:45:06 +0300

    Setup Inertia as front-end

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps({
+    laravel: String,
+    php: String,
+})
+</script>
+
+<template>
+    <h1>Hello World!</h1>
+</template>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=inter:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet" />
+
+        <!-- Inertia -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @inertiaHead
+    </head>
+    <body>
+        @inertia
+    </body>
+</html>
